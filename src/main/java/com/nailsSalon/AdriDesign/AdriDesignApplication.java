ba198eb6ed9d3b8d17222062a224bfe21c579873
Julio Feblez <chano.feblez@yahoo.com>
2024-10-03 17:21:48 -0700

    Creating Course13

--- src/main/java/com/nailsSalon/AdriDesign/AdriDesignApplication.java
+++ src/main/java/com/nailsSalon/AdriDesign/AdriDesignApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.HashMap;
 import java.util.Map;
 
-@SpringBootApplication(exclude = {org.springframework.boot.web.servlet.filter.OrderedFormContentFilter.class})
+@SpringBootApplication
 public class AdriDesignApplication {
 
 	public static void main(String[] args) {
