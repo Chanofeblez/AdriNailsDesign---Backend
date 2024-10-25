6fa6932ff84d771b03b50285253ce74cee9858fe
Julio Feblez <chano.feblez@yahoo.com>
2024-10-25 09:27:53 -0700

    Updated Course with PDF and Videos Lists

--- src/main/java/com/nailsSalon/AdriDesign/course/Course.java
+++ src/main/java/com/nailsSalon/AdriDesign/course/Course.java
@@ -25,9 +25,9 @@ public class Course {
 
   @Column(nullable = false)
   private String imagePath;  // Ruta de la imagen
-  @ElementCollection
+  @ElementCollection(fetch = FetchType.EAGER)
   private List<String> videoPaths; // Rutas de los videos
-  @ElementCollection
+  @ElementCollection(fetch = FetchType.EAGER)
   private List<String> pdfPaths; // Rutas de los archivos PDF
 
 
