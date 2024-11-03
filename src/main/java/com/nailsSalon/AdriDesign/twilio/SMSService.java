211a7ed80e904676aff8492b8a30ddd0c84134e8
Julio Feblez <chano.feblez@yahoo.com>
2024-11-02 19:55:58 -0700

    Added Twilio SMS functionality and updated environment variables

--- src/main/java/com/nailsSalon/AdriDesign/twilio/SMSService.java
+++ src/main/java/com/nailsSalon/AdriDesign/twilio/SMSService.java
@@ -1,13 +1,18 @@
 package com.nailsSalon.AdriDesign.twilio;
 
+import com.nailsSalon.AdriDesign.appointment.AppointmentService;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SMSService {
+
+  private static final Logger logger = LoggerFactory.getLogger(SMSService.class);
   @Value("${TWILIO_ACCOUNT_SID}")
   private String accountSid;
 
@@ -18,6 +23,10 @@ public class SMSService {
   private String twilioPhoneNumber;
 
   public void sendSms(String to, String message) {
+    logger.info("to: {}", to);
+    logger.info("message: {}", message);
+    logger.info("accountSid: {}", accountSid);
+    logger.info("authToken: {}", authToken);
     // Inicializar Twilio dentro del método
     Twilio.init(accountSid, authToken);
     Message.creator(
